e5e1c30f378529f8866f22ac78e216190d95bbf6
KristynaSodomkova <sodomkova.kristyna@gmail.com>
2023-03-19 10:59:27 +0100

    refactor : test negative or positive numbers only - only changed the name again for better readability

--- tests/test_zero.py
+++ tests/test_zero.py
@@ -3,7 +3,7 @@ from kata_closest_to_zero.zero import *
 list_of_num = [-7, -5, -3, -2, 0, 2, 8, 20]
 
 
-def test_negative_or_positive_num_only():
+def test_neg_and_pos_num_only():
     neg_list, pos_list = create_neg_and_pos_lists(list_of_num)
     assert all(num < 0 for num in neg_list)
     assert all(num >= 0 for num in pos_list)
