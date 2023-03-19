11d3e6c51067db9a943cf543767bef355c9eaa56
KristynaSodomkova <sodomkova.kristyna@gmail.com>
2023-03-19 10:44:55 +0100

    test for list of negative numbers passed

--- src/kata_closest_to_zero/zero.py
+++ src/kata_closest_to_zero/zero.py
@@ -2,5 +2,10 @@
 negative_list = []
 
 
-def create_neg_list():
-    return True
+def create_neg_list(list_of_num):
+    for num in list_of_num:
+        if num < 0:
+            negative_list.append(num)
+    return negative_list
+
+
