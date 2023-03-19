57a2aed63cd98f84dac1786cf4b0e62f00ee5cdb
KristynaSodomkova <sodomkova.kristyna@gmail.com>
2023-03-19 11:38:06 +0100

    test for both lists failed in test compare two values

--- tests/test_zero.py
+++ tests/test_zero.py
@@ -1,13 +1,12 @@
 from kata_closest_to_zero.zero import *
 
 list_of_num = [-7, -5, -3, -2, 0, 2, 8, 20]
-
+list_of_num2 = [-7, -5, -3, -2, 2, 8, 20]
 
 def test_neg_and_pos_num_only():
     neg_list, pos_list = create_neg_and_pos_lists(list_of_num)
     assert all(num < 0 for num in neg_list)
     assert all(num >= 0 for num in pos_list)
-
 
 def test_find_closest_neg():
     assert find_closest_neg() == -2
@@ -18,5 +17,6 @@ def test_find_closest_pos():
 
 
 def test_compare_two_values():
-    assert compare_two_values() == 0
+    assert compare_two_values(list_of_num) == 0
+    assert compare_two_values(list_of_num2) == 2
 
