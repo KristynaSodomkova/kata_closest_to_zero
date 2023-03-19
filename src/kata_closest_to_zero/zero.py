53cacbfd3b4739357e395a9c12454c868813b62a
KristynaSodomkova <sodomkova.kristyna@gmail.com>
2023-03-19 10:54:48 +0100

    same principle for negative list applied for positive list

--- src/kata_closest_to_zero/zero.py
+++ src/kata_closest_to_zero/zero.py
@@ -1,11 +1,13 @@
 
 negative_list = []
-
+positive_list = []
 
 def create_neg_list(list_of_num):
     for num in list_of_num:
         if num < 0:
             negative_list.append(num)
-    return negative_list
+        else:
+            positive_list.append(num)
+    return negative_list, positive_list
 
 
--- tests/test_zero.py
+++ tests/test_zero.py
@@ -4,7 +4,10 @@ list_of_num = [-7, -5, -3, -2, 0, 2, 8, 20]
 
 
 def test_negative_num_only():
-    result = create_neg_list(list_of_num)
-    assert all(num < 0 for num in result)
+    neg_list, pos_list = create_neg_list(list_of_num)
+    assert all(num < 0 for num in neg_list)
+    assert all(num >= 0 for num in pos_list)
+
+
 
 
