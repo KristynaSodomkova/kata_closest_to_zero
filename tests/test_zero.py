5e8e0d45dc9183f57e23c9bf29aef3890e377c2f
KristynaSodomkova <sodomkova.kristyna@gmail.com>
2023-03-19 12:12:21 +0100

    now everything works, however, there is a question, what is better - leave the function create_neg_and_pos_lists as it is, so that the list would be iterated only once, or make it into two, more transparent functions? I will personally leave it,as it is, meaning only with one iteration.

--- tests/test_zero.py
+++ tests/test_zero.py
@@ -3,17 +3,20 @@ from kata_closest_to_zero.zero import *
 list_of_num = [-7, -5, -3, -2, 0, 2, 8, 20]
 list_of_num2 = [-7, -5, -3, -2, 2, 8, 20]
 
+
 def test_neg_and_pos_num_only():
     neg_list, pos_list = create_neg_and_pos_lists(list_of_num)
     assert all(num < 0 for num in neg_list)
     assert all(num >= 0 for num in pos_list)
 
 def test_find_closest_neg():
-    assert find_closest_neg() == -2
+    negative_list, positive_list = create_neg_and_pos_lists(list_of_num)
+    assert find_closest_neg(negative_list) == -2
 
 
 def test_find_closest_pos():
-    assert find_closest_pos() == 0
+    negative_list, positive_list = create_neg_and_pos_lists(list_of_num)
+    assert find_closest_pos(positive_list) == 0
 
 
 def test_compare_two_values():
